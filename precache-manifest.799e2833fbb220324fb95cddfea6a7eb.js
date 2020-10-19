d52d062e4f80b32245ddc525d73193c77207b0d8
bturtu405 <54838965+bturtu405@users.noreply.github.com>
2020-10-19 11:43:34 +0300

    Updates

---
+++ precache-manifest.799e2833fbb220324fb95cddfea6a7eb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "87eb172d7dd66cbf44a07ebdb4b737ad",
+    "url": "/superquiz/index.html"
+  },
+  {
+    "revision": "9b4c994592869e1e72ac",
+    "url": "/superquiz/static/css/3.4018e009.chunk.css"
+  },
+  {
+    "revision": "653be66483c785e0be9a",
+    "url": "/superquiz/static/js/2.8d8d01ac.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/superquiz/static/js/2.8d8d01ac.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9b4c994592869e1e72ac",
+    "url": "/superquiz/static/js/3.7793f117.chunk.js"
+  },
+  {
+    "revision": "c338109e08ef8a40ecfb",
+    "url": "/superquiz/static/js/main.d0555c6d.chunk.js"
+  },
+  {
+    "revision": "64f59e1784d714eea6c8",
+    "url": "/superquiz/static/js/runtime-main.674f1f0d.js"
+  }
+]);
